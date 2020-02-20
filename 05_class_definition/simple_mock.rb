3cc384d570252ee653f44548e53b8c8de8105423
willnet <netwillnet@gmail.com>
2020-02-20 14:46:30 +0900

    Solve SimpleMock

--- 05_class_definition/simple_mock.rb
+++ 05_class_definition/simple_mock.rb
@@ -37,3 +37,34 @@
 # obj.imitated_method #=> true
 # obj.called_times(:imitated_method) #=> 2
 # ```
+
+module SimpleMock
+  def self.mock(obj)
+    obj.extend(SimpleMock)
+    obj
+  end
+
+  def self.new
+    obj = Object.new
+    mock(obj)
+  end
+
+  def expects(name, value)
+    define_singleton_method(name) do
+      @counter[name] += 1 if @counter&.key?(name)
+      value
+    end
+  end
+
+  def watch(name)
+    (@counter ||= {})[name] = 0
+
+    define_singleton_method(name) do
+      @counter[name] += 1 if @counter&.key?(name)
+    end
+  end
+
+  def called_times(name)
+    @counter[name]
+  end
+end
